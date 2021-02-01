1499e3960c1c096d680aeb818eb2fa16a21f9f4f
rafaelvsmello <rmellov@gmail.com>
2021-02-01 17:34:26 -0300

    Commit de atualização do projeto

---
+++ Backup/Windows/frmBackup.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace Backup
+{
+    partial class frmBackup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBackup));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.pgbProgresso = new System.Windows.Forms.ToolStripProgressBar();
+            this.lblBackup = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblData = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblHora = new System.Windows.Forms.Label();
+            this.mskHorario = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnRemoverItens = new System.Windows.Forms.Button();
+            this.btnAdicionarItens = new System.Windows.Forms.Button();
+            this.lstItens = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnProcurarDestino = new System.Windows.Forms.Button();
+            this.txtDestino = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.Color.White;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pgbProgresso,
+            this.lblBackup,
+            this.lblData});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 539);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.statusStrip1.Size = new System.Drawing.Size(858, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // pgbProgresso
+            // 
+            this.pgbProgresso.Name = "pgbProgresso";
+            this.pgbProgresso.Size = new System.Drawing.Size(100, 16);
+            this.pgbProgresso.Visible = false;
+            // 
+            // lblBackup
+            // 
+            this.lblBackup.Name = "lblBackup";
+            this.lblBackup.Size = new System.Drawing.Size(164, 17);
+            this.lblBackup.Text = "Realizando backup, aguarde...";
+            this.lblBackup.Visible = false;
+            // 
+            // lblData
+            // 
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(843, 17);
+            this.lblData.Spring = true;
+            this.lblData.Text = "#Data";
+            this.lblData.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(858, 87);
+            this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Backup";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox1.Controls.Add(this.lblHora);
+            this.groupBox1.Controls.Add(this.mskHorario);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.btnRemoverItens);
+            this.groupBox1.Controls.Add(this.btnAdicionarItens);
+            this.groupBox1.Controls.Add(this.lstItens);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnProcurarDestino);
+            this.groupBox1.Controls.Add(this.txtDestino);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(173, 118);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(544, 360);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.Location = new System.Drawing.Point(80, 312);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(39, 13);
+            this.lblHora.TabIndex = 8;
+            this.lblHora.Text = "#Hora";
+            this.lblHora.Visible = false;
+            // 
+            // mskHorario
+            // 
+            this.mskHorario.Location = new System.Drawing.Point(22, 303);
+            this.mskHorario.Mask = "00:00";
+            this.mskHorario.Name = "mskHorario";
+            this.mskHorario.Size = new System.Drawing.Size(52, 22);
+            this.mskHorario.TabIndex = 3;
+            this.mskHorario.ValidatingType = typeof(System.DateTime);
+            this.mskHorario.Leave += new System.EventHandler(this.mskHorario_Leave);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 287);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Horário:";
+            // 
+            // btnRemoverItens
+            // 
+            this.btnRemoverItens.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRemoverItens.Image = global::Backup.Properties.Resources.action_Cancel_16xMD;
+            this.btnRemoverItens.Location = new System.Drawing.Point(374, 132);
+            this.btnRemoverItens.Name = "btnRemoverItens";
+            this.btnRemoverItens.Size = new System.Drawing.Size(25, 23);
+            this.btnRemoverItens.TabIndex = 2;
+            this.btnRemoverItens.UseVisualStyleBackColor = true;
+            this.btnRemoverItens.Click += new System.EventHandler(this.btnRemoverItens_Click);
+            // 
+            // btnAdicionarItens
+            // 
+            this.btnAdicionarItens.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdicionarItens.Image = global::Backup.Properties.Resources.action_add_16xMD;
+            this.btnAdicionarItens.Location = new System.Drawing.Point(374, 104);
+            this.btnAdicionarItens.Name = "btnAdicionarItens";
+            this.btnAdicionarItens.Size = new System.Drawing.Size(25, 22);
+            this.btnAdicionarItens.TabIndex = 1;
+            this.btnAdicionarItens.UseVisualStyleBackColor = true;
+            this.btnAdicionarItens.Click += new System.EventHandler(this.btnAdicionarItens_Click);
+            // 
+            // lstItens
+            // 
+            this.lstItens.FormattingEnabled = true;
+            this.lstItens.Location = new System.Drawing.Point(22, 104);
+            this.lstItens.Name = "lstItens";
+            this.lstItens.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lstItens.Size = new System.Drawing.Size(346, 160);
+            this.lstItens.TabIndex = 4;
+            this.lstItens.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Arquivos do Backup:";
+            // 
+            // btnProcurarDestino
+            // 
+            this.btnProcurarDestino.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProcurarDestino.Image = global::Backup.Properties.Resources.magnifier_16xMD;
+            this.btnProcurarDestino.Location = new System.Drawing.Point(442, 43);
+            this.btnProcurarDestino.Name = "btnProcurarDestino";
+            this.btnProcurarDestino.Size = new System.Drawing.Size(25, 22);
+            this.btnProcurarDestino.TabIndex = 0;
+            this.btnProcurarDestino.UseVisualStyleBackColor = true;
+            this.btnProcurarDestino.Click += new System.EventHandler(this.btnProcurarDestino_Click);
+            // 
+            // txtDestino
+            // 
+            this.txtDestino.BackColor = System.Drawing.Color.White;
+            this.txtDestino.Location = new System.Drawing.Point(22, 43);
+            this.txtDestino.Name = "txtDestino";
+            this.txtDestino.ReadOnly = true;
+            this.txtDestino.Size = new System.Drawing.Size(414, 22);
+            this.txtDestino.TabIndex = 1;
+            this.txtDestino.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Destino do Backup:";
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConfirmar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConfirmar.Image = global::Backup.Properties.Resources.StatusAnnotations_Complete_and_ok_16xMD_color;
+            this.btnConfirmar.Location = new System.Drawing.Point(757, 501);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(89, 32);
+            this.btnConfirmar.TabIndex = 4;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            this.btnConfirmar.Click += new System.EventHandler(this.btnConfirmar_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "Programa em execução. Clique duas vezes no ícone para retornar.";
+            this.notifyIcon1.BalloonTipTitle = "Atenção...";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "[JR Soft] - Backup";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 48);
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmBackup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(858, 561);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(874, 600);
+            this.Name = "frmBackup";
+            this.Text = "Backup";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmBackup_FormClosing);
+            this.Load += new System.EventHandler(this.frmBackup_Load);
+            this.Resize += new System.EventHandler(this.frmBackup_Resize);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblData;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.MaskedTextBox mskHorario;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnRemoverItens;
+        private System.Windows.Forms.Button btnAdicionarItens;
+        private System.Windows.Forms.ListBox lstItens;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnProcurarDestino;
+        private System.Windows.Forms.TextBox txtDestino;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.ToolStripProgressBar pgbProgresso;
+        private System.Windows.Forms.ToolStripStatusLabel lblBackup;
+    }
+}
+
---
+++ Backup/Windows/frmBackup.cs
@@ -0,0 +1,269 @@
+﻿using Ionic.Zip;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Backup
+{
+    public partial class frmBackup : Form
+    {
+        private string nomeArquivo;        
+        List<string> arquivos = new List<string>();
+
+        private void Log(string texto)
+        {
+            StreamWriter sw = new StreamWriter(Application.StartupPath + "\\Backup.log", true);
+
+            using (sw)
+            {
+                sw.WriteLine(DateTime.Now.ToString() + texto);
+            }
+        }
+
+        private void CriarArquivoZip(List<string> arquivos, string ArquivoDestino)
+        {
+            using (ZipFile zip = new ZipFile())
+            {
+                if (lstItens.Items.Count > 0)
+                {
+                    foreach (string item in lstItens.Items)
+                    {
+
+                        if (File.Exists(item))
+                        {
+                            pgbProgresso.Visible = true;
+                            lblBackup.Visible = true;
+
+                            try
+                            {
+                                zip.SaveProgress += zipProgress;
+                                zip.AddFile(item, "");                                
+                            }
+                            catch (Exception erro)
+                            {
+                                Log(" - Erro: " + erro.Message);
+                            }
+                        }
+                        else
+                        {
+                            Log(" - Arquivo não encontrado: " + Path.GetFileName(item));
+                            return;
+                        }
+
+                    }
+
+                    try
+                    {
+                        zip.Save(ArquivoDestino);
+                        zip.Dispose();
+                    }
+                    catch (Exception erro)
+                    {
+                        Log(" - Erro: " + erro.Message);
+                    }
+
+                } 
+                else
+                {
+                    Log(" - Alerta: Nenhum arquivo selecionado");
+                }
+
+            }
+
+        }
+
+        private bool ValidarHora(string hora)
+        {
+            try
+            {
+                Convert.ToDateTime(hora);
+                Properties.Settings.Default.Horario = mskHorario.Text;
+                return true;
+            }
+            catch 
+            {
+                MessageBox.Show("Horário inválido.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                mskHorario.Focus();
+                return false;                
+            }
+        }
+
+        public frmBackup()
+        {
+            InitializeComponent();
+        }
+
+        private void frmBackup_Load(object sender, EventArgs e)
+        {
+            lblData.Text = DateTime.Now.ToLongDateString();
+            txtDestino.Text = Properties.Settings.Default.Destino;
+            mskHorario.Text = Properties.Settings.Default.Horario;
+        }
+
+        private void btnProcurarDestino_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog destino = new FolderBrowserDialog();
+            destino.Description = "Selecione o local para gravar o backup:";
+
+            if (destino.ShowDialog() == DialogResult.OK)
+            {
+                txtDestino.Text = destino.SelectedPath;
+                Properties.Settings.Default.Destino = txtDestino.Text;                
+            }
+
+        }
+
+        private void mskHorario_Leave(object sender, EventArgs e)
+        {
+            ValidarHora(mskHorario.Text);
+        }
+
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        {
+            if (lstItens.Items.Count > 0)
+            {
+                Properties.Settings.Default.Save();
+                MessageBox.Show("Configurações gravadas com sucesso.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.WindowState = FormWindowState.Minimized;
+                RealizarBackup();
+            }
+            else
+            {
+                DialogResult result = MessageBox.Show("Não existem arquivos para realizar o backup.\nDeseja continuar?", "Atenção", MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    Properties.Settings.Default.Save();
+                    this.WindowState = FormWindowState.Minimized;
+                    RealizarBackup();
+                }
+            }
+        }
+
+        private void btnAdicionarItens_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog arquivo = new OpenFileDialog();
+            arquivo.Multiselect = true;
+            arquivo.Title = "Selecione os arquivos para adicionar ao backup:";
+
+            try
+            {
+                if (arquivo.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (String file in arquivo.FileNames)
+                    {
+                        if (!lstItens.Items.Contains(file))
+                        {
+                            arquivos.Add(file);
+                            lstItens.Items.Add(file);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Arquivo(s) já selecionado(s):\n\n" + Path.GetFileName(file),
+                                "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        }
+                    }
+                }
+            }
+            catch (Exception erro)
+            {
+                Log(" - Erro: " + erro.Message);
+            }
+        }
+
+        private void frmBackup_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                notifyIcon1.Visible = true;                
+                this.ShowInTaskbar = false;
+                notifyIcon1.ShowBalloonTip(500);
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            this.WindowState = FormWindowState.Maximized;
+            this.ShowInTaskbar = true;
+        }
+
+        private void RealizarBackup()
+        {
+            try
+            {
+                nomeArquivo = txtDestino.Text + "\\MeuBackup_" + DateTime.Now.ToShortDateString().Replace("/", "") + ".zip";
+                string localNomeDestinoZIP = nomeArquivo;
+                CriarArquivoZip(arquivos, localNomeDestinoZIP);
+                pgbProgresso.Visible = false;
+                lblBackup.Visible = false;
+                MessageBox.Show("Backup realizado com sucesso.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);                
+            }
+            catch (Exception erro)
+            {
+                Log(" - Erro: " + erro.Message);
+            }
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string hora = DateTime.Now.ToLongTimeString();
+            lblHora.Text = hora;
+
+            if (hora == mskHorario.Text + ":00")
+            {
+                RealizarBackup();
+            }
+            
+        }
+
+        private void frmBackup_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Se encerrar o programa, o backup não será realizado em segundo plano.\nDeseja sair?", 
+                "Atenção", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            this.WindowState = FormWindowState.Maximized;
+            this.ShowInTaskbar = true;
+        }
+
+        private void zipProgress(object sender, SaveProgressEventArgs e)
+        {
+            if (e.EventType == ZipProgressEventType.Saving_AfterWriteEntry)
+            {
+                this.pgbProgresso.Value = e.EntriesSaved * 100 / e.EntriesTotal;
+            }
+
+            else if (e.EventType == ZipProgressEventType.Saving_Completed)
+                this.pgbProgresso.Value = 100;
+        }
+
+        // Pendente implementação da remoção dos itens
+        private void btnRemoverItens_Click(object sender, EventArgs e)
+        {
+            if(lstItens.SelectedItems.Count > 0)
+            {                
+            }
+            else
+            {
+                MessageBox.Show("Selecione um item para remover.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+    }
+}
